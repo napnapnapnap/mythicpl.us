42caf171c513c204b60ac43d09fdbef123a78f7c
napnapnapnap <napnapnapnap@users.noreply.github.com>
2026-09-22 15:49:34 +0200

    feat: display application dates for affixes

--- assets/js/getaffixes.js
+++ assets/js/getaffixes.js
@@ -26,9 +26,9 @@ function getAffixes() {
     var currentAffixesId = rotation[index];
     var row = document.getElementById(currentAffixesId);
     
-    var a1 = row.cells[0].innerHTML;
-    var a2 = row.cells[1].innerHTML;
-    var a3 = row.cells[2].innerHTML;
+    var a1 = row.cells[1].innerHTML;
+    var a2 = row.cells[2].innerHTML;
+    var a3 = row.cells[3].innerHTML;
     
     document.getElementById("thisweek").innerHTML = a1 + " " + a2 + " " + a3;
     
@@ -41,9 +41,40 @@ function getAffixes() {
     var nextRow = document.getElementById(rotation[nextIndex]);
     var wanRow = document.getElementById(rotation[wanIndex]);
     
-    document.getElementById("nextweek").innerHTML = nextRow.cells[0].innerHTML + ", " + nextRow.cells[1].innerHTML + ", " + nextRow.cells[2].innerHTML;
-    document.getElementById("weekafternext").innerHTML = wanRow.cells[0].innerHTML + ", " + wanRow.cells[1].innerHTML + ", " + wanRow.cells[2].innerHTML;
+    document.getElementById("nextweek").innerHTML = nextRow.cells[1].innerHTML + ", " + nextRow.cells[2].innerHTML + ", " + nextRow.cells[3].innerHTML;
+    document.getElementById("weekafternext").innerHTML = wanRow.cells[1].innerHTML + ", " + wanRow.cells[2].innerHTML + ", " + wanRow.cells[3].innerHTML;
     
+    
+    function formatDateRange(startDateTimestamp) {
+        var start = new Date(startDateTimestamp);
+        var end = new Date(startDateTimestamp + 7 * 24 * 60 * 60 * 1000);
+        
+        var options = { month: 'short', day: 'numeric' };
+        return "(" + start.toLocaleDateString('en-US', options) + " - " + end.toLocaleDateString('en-US', options) + ")";
+    }
+
+    var thisWeekStart = epoch + weeksDiff * msPerWeek;
+    var nextWeekStart = epoch + (weeksDiff + 1) * msPerWeek;
+    var wanWeekStart = epoch + (weeksDiff + 2) * msPerWeek;
+    
+    var elThisWeekDate = document.getElementById("date-thisweek");
+    if (elThisWeekDate) elThisWeekDate.innerText = formatDateRange(thisWeekStart);
+    
+    var elNextWeekDate = document.getElementById("date-nextweek");
+    if (elNextWeekDate) elNextWeekDate.innerText = formatDateRange(nextWeekStart);
+    
+    var elWanWeekDate = document.getElementById("date-weekafternext");
+    if (elWanWeekDate) elWanWeekDate.innerText = formatDateRange(wanWeekStart);
+
+    for (var i = 0; i < 8; i++) {
+        var weeksFromNow = (i - index + 8) % 8;
+        var rowDateStart = epoch + (weeksDiff + weeksFromNow) * msPerWeek;
+        var elRowDate = document.getElementById("date-" + rotation[i]);
+        if (elRowDate) {
+            elRowDate.innerText = formatDateRange(rowDateStart);
+        }
+    }
+
     resolveAffixes();
 }
 
